15fbb8043b0d9838a1adec65ec20329b6d5a4233
astro-friedel <friedel@illinois.edu>
2024-09-09 15:37:56 -0500

    fix install prefix for file

--- mediapipe/graphs/hand_tracking/build.cmake
+++ mediapipe/graphs/hand_tracking/build.cmake
@@ -1,5 +1,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/subgraphs/build.cmake)
 
-install(FILES hand_detection_desktop_live.pbtxt
-        DESTINATION shared/mediapipe/graphs/hand_tracking
+install(FILES ${CMAKE_CURRENT_LIST_DIR}/hand_tracking_desktop_live.pbtxt
+        DESTINATION share/mediapipe/graphs/hand_tracking
 )
