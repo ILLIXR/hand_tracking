00fc73ebf5e797ebfe1b173f53861d75a3a30763
astro-friedel <friedel@illinois.edu>
2024-12-23 12:09:30 -0600

    update for namespace change

--- openxr/interface.cpp
+++ openxr/interface.cpp
@@ -1,5 +1,6 @@
 #include "interface.h"
 #include "../hand_tracking_publisher.hpp"
+#include "illixr/math_util.hpp"
 #include "illixr/phonebook.hpp"
 #include "illixr/switchboard.hpp"
 #include "oxr_objects.h"
@@ -8,27 +9,27 @@
 
 namespace bt_int = boost::interprocess;
 
-const std::map<int, int> oxr_to_ixr_points = {{XR_HAND_JOINT_WRIST_EXT, ILLIXR::HandTracking::WRIST},
-                                              {XR_HAND_JOINT_THUMB_METACARPAL_EXT, ILLIXR::HandTracking::THUMB_CMC},
-                                              {XR_HAND_JOINT_THUMB_PROXIMAL_EXT, ILLIXR::HandTracking::THUMB_MCP},
-                                              {XR_HAND_JOINT_THUMB_DISTAL_EXT, ILLIXR::HandTracking::THUMB_IP},
-                                              {XR_HAND_JOINT_THUMB_TIP_EXT, ILLIXR::HandTracking::THUMB_TIP},
-                                              {XR_HAND_JOINT_INDEX_PROXIMAL_EXT, ILLIXR::HandTracking::INDEX_FINGER_MCP},
-                                              {XR_HAND_JOINT_INDEX_INTERMEDIATE_EXT, ILLIXR::HandTracking::INDEX_FINGER_PIP},
-                                              {XR_HAND_JOINT_INDEX_DISTAL_EXT, ILLIXR::HandTracking::INDEX_FINGER_DIP},
-                                              {XR_HAND_JOINT_INDEX_TIP_EXT, ILLIXR::HandTracking::INDEX_FINGER_TIP},
-                                              {XR_HAND_JOINT_MIDDLE_PROXIMAL_EXT, ILLIXR::HandTracking::MIDDLE_FINGER_MCP},
-                                              {XR_HAND_JOINT_MIDDLE_INTERMEDIATE_EXT, ILLIXR::HandTracking::MIDDLE_FINGER_PIP},
-                                              {XR_HAND_JOINT_MIDDLE_DISTAL_EXT, ILLIXR::HandTracking::MIDDLE_FINGER_DIP},
-                                              {XR_HAND_JOINT_MIDDLE_TIP_EXT, ILLIXR::HandTracking::MIDDLE_FINGER_TIP},
-                                              {XR_HAND_JOINT_RING_PROXIMAL_EXT, ILLIXR::HandTracking::RING_FINGER_MCP},
-                                              {XR_HAND_JOINT_RING_INTERMEDIATE_EXT, ILLIXR::HandTracking::RING_FINGER_PIP},
-                                              {XR_HAND_JOINT_RING_DISTAL_EXT, ILLIXR::HandTracking::RING_FINGER_DIP},
-                                              {XR_HAND_JOINT_RING_TIP_EXT, ILLIXR::HandTracking::RING_FINGER_TIP},
-                                              {XR_HAND_JOINT_LITTLE_PROXIMAL_EXT, ILLIXR::HandTracking::PINKY_MCP},
-                                              {XR_HAND_JOINT_LITTLE_INTERMEDIATE_EXT, ILLIXR::HandTracking::PINKY_PIP},
-                                              {XR_HAND_JOINT_LITTLE_DISTAL_EXT, ILLIXR::HandTracking::PINKY_DIP},
-                                              {XR_HAND_JOINT_LITTLE_TIP_EXT, ILLIXR::HandTracking::PINKY_TIP}
+const std::map<int, int> oxr_to_ixr_points = {{XR_HAND_JOINT_WRIST_EXT, ILLIXR::data_format::ht::WRIST},
+                                              {XR_HAND_JOINT_THUMB_METACARPAL_EXT, ILLIXR::data_format::ht::THUMB_CMC},
+                                              {XR_HAND_JOINT_THUMB_PROXIMAL_EXT, ILLIXR::data_format::ht::THUMB_MCP},
+                                              {XR_HAND_JOINT_THUMB_DISTAL_EXT, ILLIXR::data_format::ht::THUMB_IP},
+                                              {XR_HAND_JOINT_THUMB_TIP_EXT, ILLIXR::data_format::ht::THUMB_TIP},
+                                              {XR_HAND_JOINT_INDEX_PROXIMAL_EXT, ILLIXR::data_format::ht::INDEX_FINGER_MCP},
+                                              {XR_HAND_JOINT_INDEX_INTERMEDIATE_EXT, ILLIXR::data_format::ht::INDEX_FINGER_PIP},
+                                              {XR_HAND_JOINT_INDEX_DISTAL_EXT, ILLIXR::data_format::ht::INDEX_FINGER_DIP},
+                                              {XR_HAND_JOINT_INDEX_TIP_EXT, ILLIXR::data_format::ht::INDEX_FINGER_TIP},
+                                              {XR_HAND_JOINT_MIDDLE_PROXIMAL_EXT, ILLIXR::data_format::ht::MIDDLE_FINGER_MCP},
+                                              {XR_HAND_JOINT_MIDDLE_INTERMEDIATE_EXT, ILLIXR::data_format::ht::MIDDLE_FINGER_PIP},
+                                              {XR_HAND_JOINT_MIDDLE_DISTAL_EXT, ILLIXR::data_format::ht::MIDDLE_FINGER_DIP},
+                                              {XR_HAND_JOINT_MIDDLE_TIP_EXT, ILLIXR::data_format::ht::MIDDLE_FINGER_TIP},
+                                              {XR_HAND_JOINT_RING_PROXIMAL_EXT, ILLIXR::data_format::ht::RING_FINGER_MCP},
+                                              {XR_HAND_JOINT_RING_INTERMEDIATE_EXT, ILLIXR::data_format::ht::RING_FINGER_PIP},
+                                              {XR_HAND_JOINT_RING_DISTAL_EXT, ILLIXR::data_format::ht::RING_FINGER_DIP},
+                                              {XR_HAND_JOINT_RING_TIP_EXT, ILLIXR::data_format::ht::RING_FINGER_TIP},
+                                              {XR_HAND_JOINT_LITTLE_PROXIMAL_EXT, ILLIXR::data_format::ht::PINKY_MCP},
+                                              {XR_HAND_JOINT_LITTLE_INTERMEDIATE_EXT, ILLIXR::data_format::ht::PINKY_PIP},
+                                              {XR_HAND_JOINT_LITTLE_DISTAL_EXT, ILLIXR::data_format::ht::PINKY_DIP},
+                                              {XR_HAND_JOINT_LITTLE_TIP_EXT, ILLIXR::data_format::ht::PINKY_TIP}
 };
 
 const std::vector<int>invalid = {XR_HAND_JOINT_PALM_EXT,
@@ -39,24 +40,19 @@ const std::vector<int>invalid = {XR_HAND_JOINT_PALM_EXT,
 };
 
 struct ht_illixr_handle_t {
-    ILLIXR::switchboard::reader <ILLIXR::HandTracking::ht_frame> frame_reader;
-    ILLIXR::coordinates::frame ref;
+    ILLIXR::switchboard::reader <ILLIXR::data_format::ht::ht_frame> frame_reader;
+    ILLIXR::data_format::coordinates::frame ref;
     Eigen::Matrix3f convert;
     bool do_convert;
 
     explicit ht_illixr_handle_t(ILLIXR::switchboard* sb) :
-            frame_reader{sb->get_reader<ILLIXR::HandTracking::ht_frame>("ht")} {
-        auto rf = sb->get_reader<ILLIXR::coordinates::reference_frame>("coord_ref").get_ro_nullable();
-        if (rf == nullptr) {
-            ref = ILLIXR::coordinates::RIGHT_HANDED_Y_UP;
-        } else {
-            ref = rf.get()->reference;
-        }
-        if (ref == ILLIXR::coordinates::RIGHT_HANDED_Y_UP) {
-            convert = ILLIXR::coordinates::identity;
+            frame_reader{sb->get_reader<ILLIXR::data_format::ht::ht_frame>("ht")} {
+        auto rf = sb-> root_coordinates;
+        if (ref == ILLIXR::data_format::coordinates::RIGHT_HANDED_Y_UP) {
+            convert = ILLIXR::math_util::identity;
             do_convert = false;
         } else {
-            convert = ILLIXR::coordinates::conversion[ref][ILLIXR::coordinates::RIGHT_HANDED_Y_UP];
+            convert = ILLIXR::math_util::conversion[ref][ILLIXR::data_format::coordinates::RIGHT_HANDED_Y_UP];
             do_convert = true;
         }
     }
@@ -70,8 +66,8 @@ XrResult handle_create(ixr_session* session,
         handle = new ixr_hand_tracker();
         handle->session = session;
         handle->hand = info->hand;
-        handle->ixr_hand = (handle->hand == XR_HAND_LEFT_EXT) ? ILLIXR::HandTracking::LEFT_HAND
-                                                              : ILLIXR::HandTracking::RIGHT_HAND;
+        handle->ixr_hand = (handle->hand == XR_HAND_LEFT_EXT) ? ILLIXR::data_format::ht::LEFT_HAND
+                                                              : ILLIXR::data_format::ht::RIGHT_HAND;
         handle->hand_joints = info->handJointSet;
         handle->ht_handle = create_ht_illixr();
 
@@ -94,7 +90,7 @@ XrResult locate_hand(struct ixr_hand_tracker* hand_tracker, const XrHandJointsLo
         locations->isActive = false;
         return XR_SUCCESS;
     }
-    ILLIXR::points_with_units h_pts(data->hand_positions.at(static_cast<const ILLIXR::HandTracking::hand>(hand_tracker->ixr_hand)));
+    ILLIXR::data_format::points_with_units h_pts(data->hand_positions.at(static_cast<const ILLIXR::data_format::ht::hand>(hand_tracker->ixr_hand)));
     if (!h_pts.valid) {
         locations->isActive = false;
         return XR_SUCCESS;
@@ -106,7 +102,7 @@ XrResult locate_hand(struct ixr_hand_tracker* hand_tracker, const XrHandJointsLo
     if (space->space_type != OXR_SPACE_TYPE_REFERENCE_VIEW)
         h_pts.transform(data->offset_pose);
     if (locations->jointCount == 21) {
-        for (int i = ILLIXR::HandTracking::WRIST; i <= ILLIXR::HandTracking::PINKY_TIP; i++) {
+        for (int i = ILLIXR::data_format::ht::WRIST; i <= ILLIXR::data_format::ht::PINKY_TIP; i++) {
             locations->jointLocations[i].locationFlags = (h_pts[i].valid) ? (XR_SPACE_LOCATION_POSITION_VALID_BIT | XR_SPACE_LOCATION_POSITION_TRACKED_BIT) : XR_SPACE_LOCATION_POSITION_VALID_BIT;
             locations->jointLocations[i].pose.position = {h_pts[i].x(), h_pts[i].y(), h_pts[i].z()};
             locations->jointLocations[i].pose.orientation = {0., 0., 0., 0.};
